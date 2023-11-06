1b182e80b19496a48c5e4a725b2f4a5c6f8c1551
sondh2@ftech.ai <sondh2@ftech.ai>
2023-11-06 14:31:34 +0700

    test(webhook)

--- handler.go
+++ handler.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
 func validate(w http.ResponseWriter, r *http.Request) {
+	var bodyBytes []byte
+	var err error
 
+	bodyBytes, err = io.ReadAll(r.Body)
+
+	if err != nil {
+		logger.Print("Reading body failed")
+	}
+
+	w.Write(bodyBytes)
 }
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+var logger = log.New(os.Stdout, "INFO: ", log.LstdFlags)
+
 func main() {
+	var tlsCert string
+	var tlsKey string
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +23,9 @@ func main() {
 
 	r.Post("/validate", validate)
 
-	http.ListenAndServe(":3000", r)
+	flag.StringVar(&tlsCert, "tlsCertFile", "/etc/certs/cert.pem", "x509 Certificate for HTTPS.")
+	flag.StringVar(&tlsKey, "tlsKeyFile", "/etc/certs/cert.pem", "x509 Key for HTTPS.")
+
+	logger.Print("Starting server on port 3000...")
+	http.ListenAndServeTLS(":3000", tlsCert, tlsKey, r)
 }
